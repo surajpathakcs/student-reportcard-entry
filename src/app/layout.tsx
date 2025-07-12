d9ff3f1cd1ece06178df6847dbe6b7a3272c178d
surajpathak <surajpathak3430@gmail.com>
2025-07-12 17:55:03 +0545

    Move react-toastify CSS import to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ToastProvider from "@/components/ToastProvider";
+import 'react-toastify/dist/ReactToastify.css';
+import ToastProvider from "../components/ToastProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
