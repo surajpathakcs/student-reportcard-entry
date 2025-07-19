07d45e55d75232d15e099966a1e24be33187e361
surajpathak <surajpathak3430@gmail.com>
2025-07-19 16:09:38 +0545

    fix: refactored mongodb connection code to remove Promise error

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -3,14 +3,17 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGO_URI!;
 const client = new MongoClient(uri);
 
-let clientPromise: Promise<MongoClient>;
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
 
 if (!global._mongoClientPromise) {
   global._mongoClientPromise = client.connect();
 }
-clientPromise = global._mongoClientPromise;
 
-export const connectToDB = async () => {
+const clientPromise = global._mongoClientPromise;
+
+export async function connectToDB() {
   const client = await clientPromise;
   return client.db("student_json_form_data");
-};
+}
