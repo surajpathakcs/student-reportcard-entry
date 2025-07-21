c3f043a455da79ef30b2014ce22bf5db81ff65d0
surajpathak <surajpathak3430@gmail.com>
2025-07-21 11:33:50 +0545

    Revert "feat: use useState to persist the last used Registration Number in form"
    This reverts commit c20da53842ef6130e0a90070742ea4b123eea6a6.

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import React, { useState } from "react";
 import { toast } from "react-toastify"; // make sure this is imported
@@ -12,9 +11,8 @@ interface Subject {
 }
 
 const FormPage = () => {
-  const [regNo , setRegNo ] = useState("5-2-378-64-2022");
   const emptySubject = (): Subject => ({
-    Code: "CSC",
+    Code: "",
     Name: "",
     CreditHours: 3,
     GradePoint: 0,
@@ -25,6 +23,7 @@ const FormPage = () => {
     ImageName:"IMG_",
     StudentName: "",
     Level: "Bachelor",
+    RegNo: "5-2-378-64-2022",
     AcademicYear: "2080",
     Semester: "Semester - ",
     Program: "Bachelors Degree in Computer Science and Information Technology",
@@ -37,11 +36,6 @@ const FormPage = () => {
 
   const handleStudentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-
-    if(name === "RegNo"){
-      setRegNo(value);
-      return;
-    }
     setStudent((prev) => ({
       ...prev,
       [name]: value,
@@ -71,7 +65,7 @@ const handleSubmit = async (e: React.FormEvent) => {
 
   const payload = {
     ...student,
-    RegNo: regNo.trim(), // leave as string
+    RegNo: student.RegNo.trim(), // leave as string
     AcademicYear: student.AcademicYear.trim(), // leave as string
     TotalCreditHour: parseFloat(student.TotalCreditHour) || 0,
     SGPA: parseFloat(student.SGPA) || 0,
@@ -99,6 +93,7 @@ const handleSubmit = async (e: React.FormEvent) => {
       ImageName:"IMG_",
       StudentName: "",
       Level: "Bachelor",
+      RegNo: "5-2-478-64-2021",
       AcademicYear: "2080",
       Semester: "Semester - ",
       Program: "Bachelors Degree in Computer Science and Information Technology",
@@ -118,7 +113,7 @@ const handleSubmit = async (e: React.FormEvent) => {
     <div className="flex justify-center">
       <form onSubmit={handleSubmit} className="border rounded-xl p-6 w-full max-w-4xl space-y-6 bg-gray-200 m-2 shadow-md">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          {Object.entries({...student,RegNo:regNo}).map(([key, val]) => (
+          {Object.entries(student).map(([key, val]) => (
             <div key={key} className="flex flex-col">
               <label htmlFor={key} className="mb-1 font-medium">{key}</label>
               <input
